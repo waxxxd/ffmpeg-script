20f1668d6e4e126931f95a2fb181e5701971690c
Darryl Smith <darryl.smith@entelect.co.za>
2019-07-15 11:12:13 +0200

    Remove the explicit path setting

--- Cell.bat
+++ Cell.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-PATH=%PATH%;%~dp0\..\bin;
-
 SET LOCATION=%%~dpA_
 REM SET LOCATION=%%~dpA
 SET FILENAME=%%~nA.mp4
@@ -15,12 +13,13 @@ REM
 SET VIDEO_CODEC264=-c:v libx264
 IF DEFINED VIDEO_CODEC264 (
 	REM ultrafast superfast veryfast faster fast medium slow slower veryslow placebo DEFAULT=medium
-	SET PRESET=-preset slow
+	SET PRESET=-preset medium 
 	
-	REM -tune film animation grain stillimage psnr ssim fastdecode zerolatency
+	REM animation film grain stillimage psnr ssim fastdecode zerolatency
+	
 	
 	REM CRF=0-51, SANE RANGE=18-28, DEFAULT=23
-	SET CRF=-crf 28
+	SET CRF=-crf 23
 	
 	SET "VIDEO=!VIDEO_CODEC264! !PRESET! !CRF!"
 )
@@ -37,11 +36,11 @@ IF DEFINED VIDEO_CODEC265 (
 	
 )
 
-REM
+REM 
 SET SCALE=scale="640:trunc(ow/a/2)*2"
 
 REM
-SET COLOUR=eq=contrast=1.25:brightness=0.2:saturation=1:gamma=1:gamma_r=1:gamma_g=1:gamma_b=1:gamma_weight=1
+SET COLOUR=eq=contrast=1.2:brightness=0.15:saturation=1:gamma=1:gamma_r=1:gamma_g=1:gamma_b=1:gamma_weight=1
 
 IF DEFINED SCALE (
 	IF DEFINED COLOUR (
@@ -57,7 +56,8 @@ IF DEFINED SCALE (
 
 REM SET AUDIO=-c:a copy
 REM SET AUDIO -c:a aac -b:a 128k 
-REM SET AUDIO=-c:a mp3 -b:a 128k
+REM 
+SET AUDIO=-c:a mp3 -b:a 128k
 REM SET AUDIO=-c:a libvorbis -q 4.0
 
 REM SET TRACK=-map 0:v -map 0:a:0
@@ -65,19 +65,21 @@ REM SET TRACK= -map 0:a:0
 REM SET TRACK= -map 0:0
 
 REM SET START=-ss 00:22:00
-REM SET DURATION=-t 00:00:01
+REM SET DURATION=-t 00:00:10
 
 REM 
 SET AUDIO_FILTER=-af "volume=1.20"
 
+REM 
+SET A_C=-ac 1
+
 FOR %%A IN (%*) DO (
 	mkdir "%LOCATION%"
 
-	echo ffmpeg -i %%A %TITLE% -y %VIDEO% %AUDIO% %TRACK% %START% %DURATION% %AUDIO_FILTER% %VIDEO_FILTER% "%LOCATION%\%FILENAME%"
-	ffmpeg -i %%A %TITLE% -y %VIDEO% %AUDIO% %TRACK% %START% %DURATION% %AUDIO_FILTER% %VIDEO_FILTER% "%LOCATION%\%FILENAME%"
+	echo ffmpeg -i %%A %TITLE% -y %VIDEO% %AUDIO% %TRACK% %START% %DURATION% %AUDIO_FILTER% %A_C% %VIDEO_FILTER% "%LOCATION%\%FILENAME%"
+	ffmpeg -i %%A %TITLE% -y %VIDEO% %AUDIO% %TRACK% %START% %DURATION% %AUDIO_FILTER% %A_C% %VIDEO_FILTER% "%LOCATION%\%FILENAME%"
 	
 )
 
 REM shutdown -h
-REM 
-pause
+REM pause
--- Normalise.bat
+++ Normalise.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
-PATH=%PATH%;%~dp0\..\bin;
-
 SET LOCATION=%%~dpA_
 REM SET LOCATION=%%~dpA
 SET FILENAME=%%~nA.mp4
@@ -15,14 +13,15 @@ REM
 SET VIDEO_CODEC264=-c:v libx264
 IF DEFINED VIDEO_CODEC264 (
 	REM ultrafast superfast veryfast faster fast medium slow slower veryslow placebo DEFAULT=medium
-	SET PRESET=-preset medium
+	SET PRESET=-preset medium 
 	
-	REM -tune film animation grain stillimage psnr ssim fastdecode zerolatency
+	REM SET TUNE=-tune animation 
+	REM film grain stillimage psnr ssim fastdecode zerolatency
 	
 	REM CRF=0-51, SANE RANGE=18-28, DEFAULT=23
 	SET CRF=-crf 23
 	
-	SET "VIDEO=!VIDEO_CODEC264! !PRESET! !CRF!"
+	SET "VIDEO=!VIDEO_CODEC264! !PRESET! !CRF! !TUNE!"
 )
 
 REM SET VIDEO_CODEC265=-vcodec libx265
@@ -37,10 +36,10 @@ IF DEFINED VIDEO_CODEC265 (
 	
 )
 
-REM
-SET SCALE=scale="720:trunc(ow/a/2)*2"
+REM 
+SET SCALE=scale="640:trunc(ow/a/2)*2"
 
-REM SET COLOUR=eq=contrast=1.25:brightness=0.2:saturation=1:gamma=1:gamma_r=1:gamma_g=1:gamma_b=1:gamma_weight=1
+REM SET COLOUR=eq=contrast=1.15:brightness=0.1:saturation=1:gamma=1:gamma_r=1:gamma_g=1:gamma_b=1:gamma_weight=1
 
 IF DEFINED SCALE (
 	IF DEFINED COLOUR (
@@ -57,15 +56,16 @@ IF DEFINED SCALE (
 REM 
 SET AUDIO=-c:a copy
 REM SET AUDIO= -c:a aac -b:a 128k 
-REM SET AUDIO=-c:a mp3 -b:a 128k
+REM 
+SET AUDIO=-c:a mp3 -b:a 128k
 REM SET AUDIO=-c:a libvorbis -q 4.0
 
 REM SET TRACK=-map 0:v -map 0:a:0
 REM SET TRACK= -map 0:a:0
 REM SET TRACK= -map 0:0
 
-REM SET START=-ss 00:38:00
-REM SET DURATION=-t 00:00:30
+REM SET START=-ss 00:20:00
+REM SET DURATION=-t 00:00:10
 
 REM SET AUDIO_FILTER=-af "volume=1.20"
 
@@ -77,4 +77,4 @@ FOR %%A IN (%*) DO (
 	
 )
 
-pause
+REM pause
